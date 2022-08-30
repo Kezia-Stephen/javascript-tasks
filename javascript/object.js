98007f1afb724be0aa4b3724a91ae1f5637a70b8
Kezia-Stephen <77871228+Kezia-Stephen@users.noreply.github.com>
2022-08-30 16:56:08 +0530

    java script object examples

---
+++ javascript/object.js
@@ -0,0 +1,20 @@
+//object 1
+
+const person = {firstName:"Kezia", lastName:"Stephen", age:24, maritalStatus:"married"};
+console.log(person.age);
+
+//object 2
+const vehicle = {type:"two-wheeler", brand:"honda", model:"activa", fuel:"petrol"};
+console.log(vehicle);
+
+//object 3
+const mobile = {brand:"OnePlus", model:"6t", color:"matt-black", RAM:"24GB"};
+console.log(mobile);
+
+//object 4
+const book = {name:"Will", author:"Will Smith", genre:"non-fiction", ratings:10};
+console.log(book);
+
+//object 5
+const instrument = {type:"guitar", brand:"cort", kind:"acoustic", color:"matt-black"};
+console.log(instrument);
